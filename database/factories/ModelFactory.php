11d10ada5e6f249dede257a4a636e6e947fc1995
Willian Oliveira <williangd@yahoo.com.br>
2016-09-14 01:52:47 -0300

    primeiro commit

---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
+use App\User;
+
+$factory->define(User::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'username' => $faker->unique()->username,
+        'email' => $faker->safeEmail,
+        'password' => bcrypt(str_random(10)),
+        'remember_token' => str_random(10),
+    ];
+});
